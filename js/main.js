4a1ae38157217a1eb498d6a4eb173cc149ab171f
SofiaCavazos <sophcc@gmail.com>
2017-04-11 12:09:01 -0500

    firebase

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
+$(function(){
+    var tacosDB = firebase.database().ref('/usuarios');
+    //para que se actualice la informacion en cuanto algo pase.
+    tacosDB.on('value', function(data){
+        console.log("value", data.key, data.val());
+        var usuarios = data.val();
+        $.get("views/_tbTacos.html", function(template)
+             {
+            $("#rolTable-content").handlebars(template,usuarios);
+        });
 
+    });
+    //solo cuando algo cambia.
+    tacosDB.on("child_changed", function(data){
+        console.log("child_changed",data.key, data.val());
+    });
+});
+function nuevoUsuario(userId, nombre, foto, fecha, listo){
+    
+}
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady(){
     
