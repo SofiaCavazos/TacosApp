545c142c19be3b297b8fefd8b03dc81f049ae1ed
SofiaCavazos <sophcc@gmail.com>
2017-04-11 16:38:31 -0500

    icono

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var Usuarios;
-$(function(){
+$(function () {
     var tacosDB = firebase.database().ref('/usuarios');
     //para que se actualice la informacion en cuanto algo pase.
     tacosDB.on('value', function(data){
@@ -53,7 +53,7 @@ function onDeviceReady(){
                 alert(message);
             }
         });
-    })
+    });
     $(document).on("click",".avatar", function(){
         var $avatar = $(this);
         var idfb = $avatar.data("idfb");
